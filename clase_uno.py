58476e6c81cff7c455cc7f6c6042881b05437e5d
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-02 18:07:55 -0500

    Variables y operaciones en python

--- clase_uno.py
+++ clase_uno.py
@@ -5,3 +5,61 @@ Created on Thu Sep  2 12:48:56 2021
 @author: USER
 """
 
+print('Hello world')
+
+# Variables
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+# Operaciones
+
+# Suma
+a = 5
+b = 2
+s = a + b
+print(s)
+
+# Resta
+a = 5
+b = 2
+r = a - b
+print(r)
+
+# Multiplicacion
+a = 5
+b = 2
+m = a * b
+print(m)
+
+# Division
+a = 5
+b = 2
+d = a / b
+print(d)
+
+a = 5
+b = 2
+d = a // b
+print(d)
+
+# Potencia
+a = 5
+b = 2
+p = a ** b
+print(p)
+
+# Raiz
+a = 9
+r = a ** (1/2)
+print(r)
+
+import math
+r = math.sqrt(25)
+print(r)
+
