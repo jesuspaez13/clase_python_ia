895abf1f8ba064c01a09b36f2ef43318ff8afc6d
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-07 16:56:40 -0500

    Ejercicios condicionales

--- clase_dos.py
+++ clase_dos.py
@@ -32,6 +32,71 @@ print(False or False)  # False
 print(not True)  # False
 print(not False)  # True
 
+# Mas de dos condicionales al mismo tiempo
+print(True and False and True or False or True or True)  # True
+print(True and (False and True) or False or (True or True))  # True
+
+# Jerarquia de operaciones
+# 1. Parentesis y llaves
+# 2. Potencias y raices
+# 3. Multiplicacion y division
+# 4. Sumas y restas
+
+# Jerarquia de operaciones booleanas
+# 1. Parentesis y llaves
+# 2. Tabla de verdad
+
+# Estructura if
+x = -1
+if (x > 0):
+    print('1')
+else:
+    print('2')
+
+# Haga un algoritmo que dada la edad de una persona, indique si es mayor
+# o menor de edad
+edad = int(input('Digite la edad de la persona: '))
+if(edad >= 18):
+    print('Si es mayor de edad')
+else:
+    print('No es mayor de edad')
+
+# Haga un algoritmo que indique su un estudiante aprobo o reprobo una
+# asignatura, teniendo en cuenta que aprieba con minimo una
+# calificasion de 3.0 hasta 5.0
+nota = float(input('Digite su nota: '))
+if(nota >= 3.0 and nota <= 5.0):
+    print('Aprobó')
+elif(nota < 3.0 and nota > 0):
+    print('Reprobó')
+else:
+    print('La nota ingresada no es válida')
+
+# Haga un algoritmo que diga si un numero es negativo, positivo o cero
+numero = float(input('Digite el numero: '))
+if(numero > 0):
+    print('Positivo')
+elif(numero < 0):
+    print('Negativo')
+else:
+    print('El numero es cero')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
