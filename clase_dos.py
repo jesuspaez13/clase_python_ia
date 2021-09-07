5f4624edefe9e3f6931affa6c809a5cf1b12fbed
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-07 16:03:54 -0500

    Tabla de verdad, jerarquia de operaciones y condiciones

--- clase_dos.py
+++ clase_dos.py
@@ -4,6 +4,41 @@ Created on Fri Sep  3 15:59:28 2021
 
 @author: USER
 """
+# Condicionales
+
+# Tablas de verdad
+
+# Tabla del and
+# v and v = v
+# v and f = f
+# f and v = f
+# f and f = f
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# Tabla del or
+# v or v = v
+# v or f = v
+# f or v = v
+# f or f = f
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+# Negacion
+print(not True)  # False
+print(not False)  # True
+
+
+
+
+
+
+
+
 
 
 
