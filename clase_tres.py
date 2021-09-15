95a2d47567fe43fbf5cac893c67b2bd01ade29b2
Jesus Paez <jpaez13@cuc.edu.co>
2021-09-15 17:45:03 -0500

    Tuplas y set

--- clase_tres.py
+++ clase_tres.py
@@ -42,3 +42,26 @@ a = b[:]
 b = a[0:3]
 b = a[:6]
 b = a[2:]
+
+# Tuplas
+# Tipo de dato INMUTABLE y ordenado
+
+a = (1, 2, 3, 4)
+a = ()
+print(a[1])
+a = (2, 3, 4)
+b = (2, True, 'Hola', 3.4)
+c = (2, [3, 4], ['Hola', 'Mundo'], [2.3, [2.4, 2.5], 2.6])
+4 in a
+
+
+# Set
+# Mutable pero NO ordenado
+
+a = {1, 2, 3, 4}
+a = set()
+print(a[1])
+b = {2, True, 'Hola', 3.4}
+# No permite arrays en su interior
+c = {2, [3, 4], ['Hola', 'Mundo'], [2.3, [2.4, 2.5], 2.6]}
+4 in a
